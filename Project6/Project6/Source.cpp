1fbc1dcae6232e17ac83905085c200af45a0c8c8
retsnom9 <retsnom797@gmail.com>
2017-03-07 17:29:57 +0100

    lklml

--- Project6/Project6/Source.cpp
+++ Project6/Project6/Source.cpp
@@ -17,46 +17,46 @@ int main(int argc, char* args[])
 	SDL_Texture* textureb;
 	SDL_Event event;
 	SDL_Surface* surface;
-	SDL_Surface* surfaceb[1];
+	SDL_Surface* surfaceb[20];
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer);
 
-    surface = SDL_LoadBMP("ship.bmp");
-	surfaceb[0] = SDL_LoadBMP("shot.bmp");
-	surfaceb[1] = SDL_LoadBMP("shot.bmp");
+	surface = SDL_LoadBMP("ship.bmp");
+	textureb = SDL_CreateTextureFromSurface(renderer, surfaceb[a]);
+	while (a < 20)
+	{
+		surfaceb[a] = SDL_LoadBMP("shot.bmp");
+		surfaceb[a]->clip_rect.h = 20;
+		surfaceb[a]->clip_rect.w = 20;
+		surfaceb[a]->clip_rect.x = 800;
+		surfaceb[a]->clip_rect.y = 800;
 
-    texture = SDL_CreateTextureFromSurface(renderer, surface); 
-	textureb = SDL_CreateTextureFromSurface(renderer, surfaceb[0]);
-	textureb = SDL_CreateTextureFromSurface(renderer, surfaceb[1]);
+		a++;
+	}
+
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
 
 	surface->clip_rect.h = 50;
 	surface->clip_rect.w = 50;
 	surface->clip_rect.x = x;
 	surface->clip_rect.y = y;
 
-	surfaceb[0]->clip_rect.h = 20;
-	surfaceb[0]->clip_rect.w = 20;
-	surfaceb[0]->clip_rect.x = 800;
-	surfaceb[0]->clip_rect.y = 800;
-
-	surfaceb[1]->clip_rect.h = 20;
-	surfaceb[1]->clip_rect.w = 20;
-	surfaceb[1]->clip_rect.x = 800;
-	surfaceb[1]->clip_rect.y = 800;
-
-
-
 	while (close == 0)
 	{
+		if (a == 20)
+		{
+			a = 0;
+		}
+
 		if (SDL_PollEvent(&event))
 		{
 			if (event.key.keysym.sym == SDLK_ESCAPE)
 			{
 				close++;
 			}
-			
+
 			if (event.key.keysym.sym == SDLK_LEFT)
 			{
 				surface->clip_rect.x -= 10;
@@ -82,11 +82,7 @@ int main(int argc, char* args[])
 				shot = 1;
 				surfaceb[a]->clip_rect.x = (surface->clip_rect.x + 15);
 				surfaceb[a]->clip_rect.y = surface->clip_rect.y;
-
-				if (a == 1)
-				{
-					a--;
-				}
+				a++;
 			}
 		}
 
@@ -99,8 +95,8 @@ int main(int argc, char* args[])
 
 		SDL_RenderClear(renderer);
 
-		SDL_RenderCopy(renderer, textureb, NULL, &surfaceb[0]->clip_rect);
-		SDL_RenderCopy(renderer, textureb, NULL, &surfaceb[1]->clip_rect);
+		SDL_RenderCopy(renderer, textureb, NULL, &surfaceb[a]->clip_rect);
+
 		SDL_RenderCopy(renderer, texture, NULL, &surface->clip_rect);
 
 		SDL_RenderPresent(renderer);
@@ -108,6 +104,15 @@ int main(int argc, char* args[])
 		i++;
 	}
 
+	a = 0;
+
+	while (a < 20)
+	{
+		SDL_FreeSurface(surfaceb[0]);
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_FreeSurface(surface);
 	SDL_Quit();
 	return(EXIT_SUCCESS);
 }
