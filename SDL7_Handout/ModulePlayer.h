414076f03c7e8208b5f7fb69dfc7176d28e6090d
retsnom9 <retsnom797@gmail.com>
2017-04-04 18:08:30 +0200

    SDL7 done :)

---
+++ SDL7_Handout/ModulePlayer.h
@@ -0,0 +1,33 @@
+#ifndef __ModulePlayer_H__
+#define __ModulePlayer_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "p2Point.h"
+
+struct SDL_Texture;
+struct Collider;
+
+class ModulePlayer : public Module
+{
+public:
+	ModulePlayer();
+	~ModulePlayer();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+	void OnCollision(Collider* c1, Collider* c2);
+
+public:
+
+	Collider* collider = nullptr;
+	SDL_Texture* graphics = nullptr;
+	Animation* current_animation = nullptr;
+	Animation idle;
+	Animation up;
+	Animation down;
+	iPoint position;
+};
+
+#endif
